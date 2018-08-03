dcae7d7d7f155bf3cd5672195b1cd313bf6f880a
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2018-08-02 21:30:09 -0400

    Created Demo project

--- COMP123-S2017-Lesson12B-LearnWindowForm/DemoForm.cs
+++ COMP123-S2017-Lesson12B-LearnWindowForm/DemoForm.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Bishnu Khanal
+ * Student ID: 
+ * Date: August 1st 2018
+ * Des: Demo Application to Showcase form and UI controls
+ * Version: 0.1- Project was created
+ * */
 namespace COMP123_S2017_Lesson12B_LearnWindowForm
 {
-    public partial class Form1 : Form
+    public partial class DemoForm : Form
     {
-        public Form1()
+        public DemoForm()
         {
             InitializeComponent();
         }
--- COMP123-S2017-Lesson12B-LearnWindowForm/Program.cs
+++ COMP123-S2017-Lesson12B-LearnWindowForm/Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Bishnu Khanal
+ * Student ID: 
+ * Date: August 1st 2018
+ * Des: Demo Application to Showcase form and UI controls
+ * Version: 0.1- Project was created
+ * */
 namespace COMP123_S2017_Lesson12B_LearnWindowForm
 {
     static class Program
@@ -16,7 +22,7 @@ namespace COMP123_S2017_Lesson12B_LearnWindowForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DemoForm());
         }
     }
 }
